4906be6541d53f82bc17e063bb95a90e4d3406a1
TempoyakBelacan <andikarizkipradanaa@gmail.com>
2023-02-03 20:34:24 +0700

    First commit

---
+++ app/Http/Controllers/QuestionController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Question;
+
+class QuestionController extends Controller
+{
+    public function index()
+    {
+        return Question::all();
+    }
+
+    public function store(Request $request)
+    {
+        $question = Question::create($request->all());
+        return response()->json($question, 201);
+    }
+
+    public function show($id)
+    {
+        $question = Question::findOrFail($id);
+        return response()->json($question);
+    }
+
+    public function update(Request $request, $id)
+    {
+        $question = Question::findOrFail($id);
+        $question->update($request->all());
+        return response()->json($question, 200);
+    }
+
+    public function delete($id)
+    {
+        Question::findOrFail($id)->delete();
+        return response()->json(null, 204);
+    }
+}
+
---
+++ app/Models/Question.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Jenssegers\Mongodb\Eloquent\Model;
+
+class Question extends Model
+{
+    use HasFactory;
+    protected $connection = 'mongodb';
+    protected $collection = 'questions';
+    protected $fillable = [
+        'question', 'type', 'options', 'conditional'
+    ];
+}
