33bc74f4f471fac957ee7ea66b8780bcd9754a29
TempoyakBelacan <andikarizkipradanaa@gmail.com>
2023-02-06 01:02:25 +0700

    Fetch all question and pass into question view

--- app/Http/Controllers/AdminController.php
+++ app/Http/Controllers/AdminController.php
@@ -2,6 +2,9 @@
 
 namespace App\Http\Controllers;
 
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
 class AdminController extends Controller
 {
     public function index()
@@ -11,6 +14,26 @@ class AdminController extends Controller
         ];
         
         return view('admin.index', $data);
+    }
+
+    public function question()
+    {
+        // Store current route name, since after Route::dispatch, route name is not accessible
+        $currentRoute = Route::currentRouteName();
+
+        // Accessing REST API
+        $response = Request::create('/api/questions');
+
+        // Decode the json into and make it into array
+        $questions = json_decode(Route::dispatch($response)->getContent(), true);
+
+        $data = [
+            'title' => 'Question',
+            'currentRoute' => $currentRoute,
+            'questions' => $questions
+        ];
+        
+        return view('admin.question.question', $data);
     }
 }
 
