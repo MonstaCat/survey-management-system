ab71b216f7ad13c17233934128df09f3e0cbac1b
TempoyakBelacan <andikarizkipradanaa@gmail.com>
2023-02-05 22:38:40 +0700

    Adding datatables library

--- resources/views/admin/_partials/scripts.blade.php
+++ resources/views/admin/_partials/scripts.blade.php
@@ -9,8 +9,11 @@
 <script src="{{ asset('sb-admin-2/js/sb-admin-2.min.js') }}"></script>
 
 <!-- Page level plugins -->
+<script src="{{ asset('sb-admin-2/vendor/datatables/jquery.dataTables.min.js') }}"></script>
+<script src="{{ asset('sb-admin-2/vendor/datatables/dataTables.bootstrap4.min.js') }}"></script>
 <script src="{{ asset('sb-admin-2/vendor/chart.js/Chart.min.js') }}"></script>
 
 <!-- Page level custom scripts -->
 <script src="{{ asset('sb-admin-2/js/demo/chart-area-demo.js') }}"></script>
 <script src="{{ asset('sb-admin-2/js/demo/chart-pie-demo.js') }}"></script>
+<script src="{{ asset('sb-admin-2/js/demo/datatables-demo.js') }}"></script>
