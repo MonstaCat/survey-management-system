905278cd0549e857d302064fb80cab7e0f4d8381
triiipooong <63167983+triiipooong@users.noreply.github.com>
2023-03-02 01:40:43 +0700

    changing form login #2

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,82 +1,93 @@
-<section class="vh-100" style="background-color: #728FCE;">
+<section class="vh-100" style="background-color: #52E0E7;">
 @extends('layouts.app')
 
 @section('content' )
 
-<div class="container py-5 h-100">
-    <div class="row d-flex justify-content-center align-items-center h-100">
-      <div class="col-12 col-md-8 col-lg-6 col-xl-5">
-        <div class="card bg-light text-dark" style="border-radius: 1rem;">
-        <div class="card-header fw-bold mb-2 text-uppercase">{{ __('Login') }}</div>
-          
+<!-- Section: Design Block -->
+  <!-- Jumbotron -->
+  <div class="px-4 py-5 px-md-5 text-center text-lg-start" style="background-color: #52E0E7;">
+    <div class="container">
+      <div class="row gx-lg-5 align-items-center">
+        <div class="col-lg-6 mb-5 mb-lg-0">
+          <h1 class="my-5 display-3 fw-bold ls-tight">
+            Selamat Datang <br />
+            <span class="text-primary">di platform survey</span>
+          </h1>
+          <p style="color: hsl(217, 10%, 50.8%)">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit.
+            Eveniet, itaque accusantium odio, soluta, corrupti aliquam
+            quibusdam tempora at cupiditate quis eum maiores libero
+            veritatis? Dicta facilis sint aliquid ipsum atque?
+          </p>
+        </div>
 
-        <div class="card-body p-5 text-center">
-            <div class="mb-md-5 mt-md-4 pb-5">
-                    <form method="POST" action="{{ route('login') }}">
+        <div class="col-lg-6 mb-5 mb-lg-0">
+          <div class="card">
+          <div class="card-header" style="background-color: #AED6F1;">{{ __('Login') }}</div>
+            <div class="card-body py-5 px-md-5">
+            <form method="POST" action="{{ route('login') }}">
                         @csrf
-                    
-                        <div class="row mb-3">
-                            <label for="email" class="col-md-4 flex-column col-form-label">{{ __('Email Address') }}</label>
 
-                            
-                                <div class="form-outline form-white mb-4">
-                                    <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
+                <!-- Email input -->
+                <div class="form-outline mb-4">
+                <input id="email" type="email" class="form-control @error('email') is-invalid @enderror" name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
 
-                                    @error('email')
-                                        <span class="invalid-feedback" role="alert">
-                                            <strong>{{ $message }}</strong>
-                                        </span>
-                                    @enderror
-                                </div>
-                            </div>
-                        
+                        @error('email')
+                            <span class="invalid-feedback" role="alert">
+                                <strong>{{ $message }}</strong>
+                            </span>
+                        @enderror
 
-                        <div class="row mb-3">
-                            <label for="password" class="col-md-4 flex-column col-form-label">{{ __('Password') }}</label>
+                  <label for="email" class="col-md-4 flex-column col-form-label">{{ __('Email Address') }}</label>
+                </div>
 
-                                <div class="form-group">
-                                    <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                <!-- Password input -->
+                <div class="form-outline mb-4">
+                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
-                                        @error('password')
-                                            <span class="invalid-feedback" role="alert">
-                                                <strong>{{ $message }}</strong>
-                                            </span>
-                                        @enderror
-                                </div>
-                        </div>
+                    @error('password')
+                        <span class="invalid-feedback" role="alert">
+                            <strong>{{ $message }}</strong>
+                        </span>
+                    @enderror
+                
+                  <label for="password" class="col-md-4 flex-column col-form-label">{{ __('Password') }}</label>
+                </div>
 
-                        <div class="row mb-3">
-                            <div class="col-md-6">
-                                <div class="form-check">
-                                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
-
-                                    <label class="form-check-label" for="remember">
+                <!-- Checkbox -->
+                <div class="form-check d-flex justify-content-center mb-4">
+                    <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
+                    <label class="form-check-label" for="remember">
                                         {{ __('Remember Me') }}
-                                    </label>
-                                </div>
-                            </div>
-                        </div>
+                    </label>
+                </div>
 
-                        <div class="row mb-0">
-                            <div class="col-md-10 flex-column col-form-label">
-                                <div class="form-group">
-                                    <button type="submit"  class="btn btn-info text-light">
-                                        {{ __('Login') }}
-                                    </button>
-                                    <a href="{{ route('login.google') }}" class="btn btn-outline-info">{{ __('Login with Google') }}</a>
+                <!-- Submit button -->
+                <div class="text-center">
+                    <button type="submit"  class="btn btn-info btn-lg btn-block text-light mb-4">
+                        {{ __('Login') }}
+                    </button>
+                </div>
+
+                <!-- Register buttons -->
+                <div class="text-center">
+                  <p>or</p>
+                  <a href="{{ route('login.google') }}" class="btn btn-outline-info">{{ __('Login with Google') }}</a>
                                     @if (Route::has('password.request'))
+                                    <br>
                                         <a class="btn btn-link" href="{{ route('password.request') }}">
                                             {{ __('Forgot Your Password?') }}
                                         </a>
                                     @endif
-                                </div>
-                            </div>
-                        </div>
-                    </form>
                 </div>
+              </form>
             </div>
+          </div>
         </div>
+      </div>
     </div>
-</div>
+  </div>
+  <!-- Jumbotron -->
 </section>
+<!-- Section: Design Block -->
 @endsection
