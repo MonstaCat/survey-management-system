0e78604ab83c486d20d765a8f88b0cb9aa2324d1
TempoyakBelacan <andikarizkipradanaa@gmail.com>
2023-05-16 00:45:46 +0700

    Modify template engine

--- resources/views/user/_partials/default.blade.php
+++ resources/views/user/_partials/default.blade.php
@@ -5,21 +5,25 @@
 
 <body class="g-sidenav-show  bg-gray-200">
 
-@include('user._partials.sidebar')
+	@include('user._partials.sidebar')
 
-  <main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
+	<main class="main-content position-relative max-height-vh-100 h-100 border-radius-lg ">
 
-    @include('user._partials.navbar')
+		@include('user._partials.navbar')
 
-      @yield('content')
-</div>
-    @include('user._partials.footer')
+		<div class="container-fluid py-4">
 
-  </main>
+			@yield('content')
 
-    @include('user._partials.plugins')
+			@include('user._partials.footer')
 
-    @include('user._partials.scripts')
+		</div>
+
+	</main>
+
+	@include('user._partials.plugins')
+
+	@include('user._partials.scripts')
 </body>
 
 </html>
