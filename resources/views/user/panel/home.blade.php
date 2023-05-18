77b4a7131576f041d7ae49cc7308a12eb7ed9d6b
Dimas Nughadi <dimas19ti@mahasiswa.pcr.ac.id>
2023-05-18 20:51:38 +0700

    padding botom

--- resources/views/user/panel/home.blade.php
+++ resources/views/user/panel/home.blade.php
@@ -6,7 +6,7 @@
     style="background-image: url('https://images.unsplash.com/photo-1531512073830-ba890ca4eba2?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1920&q=80');">
     <span class="mask  bg-gradient-primary  opacity-6"></span>
 </div>
-<div class="card card-body mx-1 mx-md-4 mt-n6">
+<div class="card card-body mx-1 mx-md-4 mt-n6" style="padding-bottom: 50px;">
     <div class="row gx-4 mb-2">
         <div class="col-auto">
             <div class="avatar avatar-xl position-relative">
